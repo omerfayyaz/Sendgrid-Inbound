7f183db9d10abc318f9b191d780eca36c4a3b725
omerfayyaz <omerfayyaz.engr@gmail.com>
2023-02-11 02:32:59 +0500

    route correction

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostsController::class, 'index'])->name('index');
 Route::get('/posts/{id}', [PostsController::class, 'show'])->name('view-post');
 Route::post('/posts/receive-email-response', [PostsController::class, 'receiveEmailResponse']);
-Route::get('send-email', [PostsController::class, 'sendMails']);
+Route::get('send-email/{id}', [PostsController::class, 'sendMails']);
